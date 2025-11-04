00cc4e12615c4f82b809e7796654f7b596d49c19
Mohd-Kaif32 <mohdkaifjhanda466@gmail.com>
2025-11-04 18:57:58 +0530

    final

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_URL || ""; // empty for local dev if you want to use proxy
 console.log("backend base url",API_BASE);
 const api = axios.create({
-  baseURL: API_BASE,
+  baseURL:process.env.REACT_APP_API_URL|| API_BASE,
   withCredentials:true
 });
 
