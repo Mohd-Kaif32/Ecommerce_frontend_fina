b2186b40bad194eec215038f0693501381091a59
Mohd-Kaif32 <mohdkaifjhanda466@gmail.com>
2025-11-04 18:45:59 +0530

    fix: corrected orderSuccess.css import and path for Render buildd

--- src/component/Cart/OrderSuccess.js
+++ src/component/Cart/OrderSuccess.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import "./orderSuccessFix.css";
+import "./orderSuccessFixed.css";
 import { Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
